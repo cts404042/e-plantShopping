2136c5c46e0c3b97118566e451880d1592a76eb3
keerthi3392 <keerthika.s@telliant.net>
2025-08-26 18:39:43 +0530

    functions implemented

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,44 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
-  };
+  return cart.reduce((total, item) => {
+    // if item.cost is like "#15" or "$20", clean it first
+    const price = parseFloat(item.cost.toString().replace(/[^0-9.]/g, ""));
+    return total + price * item.quantity;
+  }, 0);
+};
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e);
   };
+
+//   const handleCheckoutShopping = (e) => {
+//   alert('Functionality to be added for future reference');
+// };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity > 1)
+    {
+   dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
+    }
+    else if(item.quantity === 1) {
+      dispatch(removeItem(item.name))
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return ((parseFloat(item.cost.substring(1))) * (item.quantity))
   };
 
   return (
